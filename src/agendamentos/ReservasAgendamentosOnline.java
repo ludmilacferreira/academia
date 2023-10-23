d0a8d548ae0366d2c3785225ca226a6fc23cb3c5
ludmilacferreira <136506322+ludmilacferreira@users.noreply.github.com>
2023-10-23 15:38:37 -0300

    Update ReservasAgendamentosOnline.java

--- src/agendamentos/ReservasAgendamentosOnline.java
+++ src/agendamentos/ReservasAgendamentosOnline.java
@@ -4,24 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReservasAgendamentosOnline {
-    private List<String> reservas;
-
-    public ReservasAgendamentosOnline() {
-        this.reservas = new ArrayList<>();
+   public void (String instrutor, String horario) {
+        System.out.println("Aula agendada para o instrutor " + instrutor + " no horário " + horario);
     }
 
-    public void fazerReserva(String horario) {
-        reservas.add(horario);
-        System.out.println("Reserva feita para o horário: " + horario);
-    }
-
-    public void cancelarReserva(String horario) {
-        if (reservas.contains(horario)) {
-            reservas.remove(horario);
-            System.out.println("Reserva cancelada para o horário: " + horario);
-        } else {
-            System.out.println("Nenhuma reserva encontrada para o horário especificado.");
-        }
+    public void (String instrutor, String horario) {
+        System.out.println("Aula cancelada para o instrutor " + instrutor + " no horário " + horario);
     }
 }
-
