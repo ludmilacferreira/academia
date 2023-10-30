bf331ca50e6cbb3aa0f251ca169263d2d8c270c9
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-10-30 16:55:56 -0300

    Update Menu.java
    adicionando o código de Menu

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,4 +1,23 @@
 package view;
 
 public class Menu {
+
+    public static void mostrarMenu() {
+        System.out.println("Escolha uma opção:");
+        System.out.println("1. Mostrar Controle de Acesso");
+        System.out.println("2. Mostrar Reserva e Agendamento Online");
+        System.out.println("3. Mostrar Gerenciamento de Aulas");
+        System.out.println("4. Mostrar Tela de Cadastro");
+        System.out.println("5. Mostrar Comunicação com Membros");
+        System.out.println("6. Mostrar Controle de Estoque");
+        System.out.println("7. Mostrar Gerenciamento de Instrutores");
+        System.out.println("8. Mostrar Integrações com Sistema de Pagamento");
+        System.out.println("9. Mostrar Controle de Pagamento");
+        System.out.println("10. Mostrar Relatórios e Estatísticas");
+        System.out.println("11. Mostrar Segurança");
+        System.out.println("12. Mostrar UI");
+        System.out.println("13. Mostrar UX");
+        System.out.println("14. Sair");
+        System.out.println("Digite uma opção:");
+    }
 }
