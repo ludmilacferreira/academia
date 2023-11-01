562c5a690749ad76d3c2aa2c4e0b8ff9e380dc40
Larissa-Morais <131203914+Larissa-Morais@users.noreply.github.com>
2023-11-01 17:41:22 -0300

    Update MenuPrincipal.java
    apagando o que não era mais necessário, mudando Menu para MenuPrincipal e corrigindo a númeração

--- src/view/MenuPrincipal.java
+++ src/view/MenuPrincipal.java
@@ -1,8 +1,8 @@
 package view;
 
-public class Menu {
+public class MenuPrincipal {
 
-    public static void mostrarMenu() {
+    public static void mostrarMenuPrincipal() {
         System.out.println("Escolha uma opção:");
         System.out.println("1. Mostrar Controle de Acesso");
         System.out.println("2. Mostrar Reserva e Agendamento Online");
@@ -11,13 +11,10 @@ public class Menu {
         System.out.println("5. Mostrar Comunicação com Membros");
         System.out.println("6. Mostrar Controle de Estoque");
         System.out.println("7. Mostrar Gerenciamento de Instrutores");
-        System.out.println("8. Mostrar Integrações com Sistema de Pagamento");
-        System.out.println("9. Mostrar Controle de Pagamento");
-        System.out.println("10. Mostrar Relatórios e Estatísticas");
-        System.out.println("11. Mostrar Segurança");
-        System.out.println("12. Mostrar UI");
-        System.out.println("13. Mostrar UX");
-        System.out.println("14. Sair");
+        System.out.println("8. Mostrar Controle de Pagamento");
+        System.out.println("9. Mostrar Relatórios e Estatísticas");
+        System.out.println("10. Mostrar Segurança");
+        System.out.println("11. Sair");
         System.out.println("Digite uma opção:");
     }
 }
