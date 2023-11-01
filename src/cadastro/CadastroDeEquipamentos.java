031e8a0ecd11302bfbf3b64536e5b57e2f953b34
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-11-01 00:00:29 -0300

    Update CadastroDeEquipamentos.java
    adicionando código da classe cadastro de equipamentos

--- src/cadastro/CadastroDeEquipamentos.java
+++ src/cadastro/CadastroDeEquipamentos.java
@@ -1,1 +1,57 @@
+package cadastro;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroDeEquipamentos {
+
+    public static class Equipamento {
+        private String nome;
+        private String tipo;
+
+        public Equipamento(String nome, String tipo) {
+            this.nome = nome;
+            this.tipo = tipo;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getTipo() {
+            return tipo;
+        }
+    }
+
+    public static class CadastroEquipamentos {
+        private List<Equipamento> listaEquipamentos;
+
+        public CadastroEquipamentos() {
+            this.listaEquipamentos = new ArrayList<>();
+        }
+
+        public void adicionarEquipamento(Equipamento equipamento) {
+            listaEquipamentos.add(equipamento);
+        }
+
+        public void mostrarEquipamentos() {
+            for (Equipamento equipamento : listaEquipamentos) {
+                System.out.println("Nome: " + equipamento.getNome() + ", Tipo: " + equipamento.getTipo());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CadastroEquipamentos cadastroDeEquipamentos = new CadastroEquipamentos();
+
+        Equipamento equipamento1 = new Equipamento("Esteira", "Cardio");
+        Equipamento equipamento2 = new Equipamento("Bicicleta Ergométrica", "Cardio");
+        Equipamento equipamento3 = new Equipamento("Haltere", "Musculação");
+
+        cadastroDeEquipamentos.adicionarEquipamento(equipamento1);
+        cadastroDeEquipamentos.adicionarEquipamento(equipamento2);
+        cadastroDeEquipamentos.adicionarEquipamento(equipamento3);
+
+        cadastroDeEquipamentos.mostrarEquipamentos();
+    }
+}
