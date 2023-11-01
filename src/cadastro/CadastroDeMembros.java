afd26f86478d85c2b27a0fe165fcaff0aa698ecc
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-10-31 23:55:18 -0300

    Update CadastroDeMembros.java
    por algum motivo, o código antigo não tava rodando no intellij, então fiz algumas mudanças

--- src/cadastro/CadastroDeMembros.java
+++ src/cadastro/CadastroDeMembros.java
@@ -1,9 +1,48 @@
 package cadastro;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CadastroDeMembros {
+
+    public static class Membro {
+        private String nome;
+        private String tipoMembro;
+
+        public Membro(String nome, String tipoMembro) {
+            this.nome = nome;
+            this.tipoMembro = tipoMembro;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getTipoMembro() {
+            return tipoMembro;
+        }
+    }
+
+    public static class CadastroMembros {
+        private List<Membro> listaMembros;
+
+        public CadastroMembros() {
+            this.listaMembros = new ArrayList<>();
+        }
+
+        public void adicionarMembro(Membro membro) {
+            listaMembros.add(membro);
+        }
+
+        public void mostrarMembros() {
+            for (Membro membro : listaMembros) {
+                System.out.println("Nome: " + membro.getNome() + ", Tipo de Membro: " + membro.getTipoMembro());
+            }
+        }
+    }
+
     private Map<Integer, Membro> membros;
 
     public CadastroDeMembros() {
@@ -11,9 +50,9 @@ public class CadastroDeMembros {
     }
 
     public void cadastrarMembro(int id, String nome, PlanoPagamento plano) {
-        Membro membro = new Membro(id, nome, plano);
+        Membro membro = new Membro(nome, plano.toString());
         membros.put(id, membro);
-        System.out.println("Membro cadastrado com sucesso: " + membro);
+        System.out.println("Membro cadastrado com sucesso: " + membro.getNome() + " com o plano de pagamento " + membro.getTipoMembro());
     }
 
     public Membro encontrarMembro(int id) {
@@ -28,45 +67,40 @@ public class CadastroDeMembros {
         if (membroExiste(id)) {
             Membro membro = encontrarMembro(id);
             System.out.println("Detalhes do membro:");
-            System.out.println("ID: " + membro.getId());
+            System.out.println("ID: " + id);
             System.out.println("Nome: " + membro.getNome());
-            System.out.println("Plano de Pagamento: " + membro.getPlano());
+            System.out.println("Plano de Pagamento: " + membro.getTipoMembro());
         } else {
             System.out.println("Membro não encontrado.");
         }
     }
-}
 
-class Membro {
-    private int id;
-    private String nome;
-    private PlanoPagamento plano;
+    public static void main(String[] args) {
+        CadastroMembros cadastroDeMembros = new CadastroMembros();
 
-    public Membro(int id, String nome, PlanoPagamento plano) {
-        this.id = id;
-        this.nome = nome;
-        this.plano = plano;
+        Membro membro1 = new Membro("Ludmilla", "Premium");
+        Membro membro2 = new Membro("Caio", "Regular");
+        Membro membro3 = new Membro("Clara", "Premium");
+
+        cadastroDeMembros.adicionarMembro(membro1);
+        cadastroDeMembros.adicionarMembro(membro2);
+        cadastroDeMembros.adicionarMembro(membro3);
+
+        cadastroDeMembros.mostrarMembros();
+
+        CadastroDeMembros cadastro = new CadastroDeMembros();
+
+        cadastro.cadastrarMembro(1, "Ludmilla", PlanoPagamento.ANUAL);
+        cadastro.cadastrarMembro(2, "Caio", PlanoPagamento.MENSAL);
+        cadastro.cadastrarMembro(3, "Clara", PlanoPagamento.SEMESTRAL);
+
+        cadastro.mostrarDetalhesMembro(1);
+        cadastro.mostrarDetalhesMembro(2);
+        cadastro.mostrarDetalhesMembro(3);
     }
 
-    public int getId() {
-        return id;
+    public enum PlanoPagamento {
+        MENSAL, TRIMESTRAL, SEMESTRAL, ANUAL
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public PlanoPagamento getPlano() {
-        return plano;
-    }
-
-    @Override
-    public String toString() {
-        return "ID: " + id + ", Nome: " + nome + ", Plano de Pagamento: " + plano;
-    }
-}
-
-enum PlanoPagamento {
-    MENSAL, TRIMESTRAL, SEMESTRAL, ANUAL
 }
 
