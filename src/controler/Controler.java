b74a1724d92408fe4009928aa81985b538566b37
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-11-01 00:01:38 -0300

    Update Controler.java
    atualizando código de controler

--- src/controler/Controler.java
+++ src/controler/Controler.java
@@ -1,16 +1,22 @@
 package controler;
 
 import acesso.ControleDeAcesso;
+import cadastro.*;
 import comunicacao.ComunicacaoComMembros;
+
+
+import java.util.Scanner;
 
 public class Controler {
 
     private ControleDeAcesso controleDeAcesso;
     private ComunicacaoComMembros comunicacaoComMembros;
+    private CadastroDeFuncionarios cadastroDeFuncionarios;
 
     public Controler() {
         this.controleDeAcesso = new ControleDeAcesso();
         this.comunicacaoComMembros = new ComunicacaoComMembros();
+        this.cadastroDeFuncionarios = new CadastroDeFuncionarios();
     }
 
     public void mostrarControleDeAcesso() {
@@ -34,7 +40,78 @@ public class Controler {
     }
 
     public void mostrarTelaDeCadastro() {
-        System.out.println("Implemente a funcionalidade de Tela de Cadastro aqui.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem-vindo à tela de cadastro. Escolha uma opção:");
+        System.out.println("1. Cadastro de Equipamentos");
+        System.out.println("2. Cadastro de Funcionários");
+        System.out.println("3. Cadastro de Membros");
+        System.out.print("Digite o número da opção desejada: ");
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                realizarCadastroDeEquipamentos();
+                break;
+            case 2:
+                realizarCadastroDeFuncionarios();
+                break;
+            case 3:
+               realizarCadastroDeMembros();
+                break;
+            default:
+                System.out.println("Opção inválida. Por favor, tente novamente.");
+        }
+    }
+    private void realizarCadastroDeEquipamentos() {
+        CadastroDeEquipamentos.Equipamento equipamento1 = new CadastroDeEquipamentos.Equipamento("Esteira", "Cardio");
+        CadastroDeEquipamentos.Equipamento equipamento2 = new CadastroDeEquipamentos.Equipamento("Bicicleta Ergométrica", "Cardio");
+        CadastroDeEquipamentos.Equipamento equipamento3 = new CadastroDeEquipamentos.Equipamento("Haltere", "Musculação");
+
+        CadastroDeEquipamentos.CadastroEquipamentos cadastroDeEquipamentos = new CadastroDeEquipamentos.CadastroEquipamentos();
+        cadastroDeEquipamentos.adicionarEquipamento(equipamento1);
+        cadastroDeEquipamentos.adicionarEquipamento(equipamento2);
+        cadastroDeEquipamentos.adicionarEquipamento(equipamento3);
+
+        cadastroDeEquipamentos.mostrarEquipamentos();
+    }
+
+    private void realizarCadastroDeFuncionarios() {
+        CadastroDeFuncionarios.Funcionario funcionario1 = cadastroDeFuncionarios.new Funcionario("João", "Faxineiro");
+        CadastroDeFuncionarios.Funcionario funcionario2 = cadastroDeFuncionarios.new Funcionario("Larissa", "Recepcionista");
+        CadastroDeFuncionarios.Funcionario funcionario3 = cadastroDeFuncionarios.new Funcionario("Pedro", "Instrutor");
+
+        cadastroDeFuncionarios.adicionarFuncionario(funcionario1);
+        cadastroDeFuncionarios.adicionarFuncionario(funcionario2);
+        cadastroDeFuncionarios.adicionarFuncionario(funcionario3);
+
+        cadastroDeFuncionarios.mostrarFuncionarios();
+    }
+
+    private void realizarCadastroDeMembros() {
+       CadastroDeMembros.Membro membro1 = new CadastroDeMembros.Membro("Ludmilla", "Premium");
+        CadastroDeMembros.Membro membro2 = new CadastroDeMembros.Membro("Caio", "Regular");
+        CadastroDeMembros.Membro membro3 = new CadastroDeMembros.Membro("Clara", "Premium");
+
+        CadastroDeMembros.CadastroMembros cadastroDeMembros = new CadastroDeMembros.CadastroMembros();
+        cadastroDeMembros.adicionarMembro(membro1);
+        cadastroDeMembros.adicionarMembro(membro2);
+        cadastroDeMembros.adicionarMembro(membro3);
+
+        cadastroDeMembros.mostrarMembros();
+
+        CadastroDeMembros cadastro = new CadastroDeMembros();
+
+        cadastro.cadastrarMembro(1, "Ludmilla", CadastroDeMembros.PlanoPagamento.ANUAL);
+        cadastro.cadastrarMembro(2, "Caio", CadastroDeMembros.PlanoPagamento.MENSAL);
+        cadastro.cadastrarMembro(3, "Clara", CadastroDeMembros.PlanoPagamento.SEMESTRAL);
+
+        cadastro.mostrarDetalhesMembro(1);
+        cadastro.mostrarDetalhesMembro(2);
+        cadastro.mostrarDetalhesMembro(3);
+    }
+
+    private void realizarCadastroDeProdutos() {
+        System.out.println("Implemente a funcionalidade de Cadastro de Produtos aqui.");
     }
 
     public void mostrarComunicacaoComMembros() {
