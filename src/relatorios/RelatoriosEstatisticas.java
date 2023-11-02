dff9917a67ffa07d4694cb4594c78b85633560a6
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-11-02 19:22:25 -0300

    Update RelatoriosEstatisticas.java
    corrigindo porque antes não tava funcionando

--- src/relatorios/RelatoriosEstatisticas.java
+++ src/relatorios/RelatoriosEstatisticas.java
@@ -2,53 +2,67 @@ package relatorios;
 
 import java.util.ArrayList;
 import java.util.List;
-public class RelatoriosEstatisticas {
-    private String nome;
-    private int frequencia;
-    private double mensalidade;
-    public Membro(String nome, int frequencia, double mensalidade) {
-        this.nome = nome;
-        this.frequencia = frequencia;
-        this.mensalidade = mensalidade;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public int getFrequencia() {
-        return frequencia;
-    }
-    public double getMensalidade() {
-        return mensalidade;
-    }
-}
-    public class Academia {
-    private List<Membro> membros = new ArrayList<>();
-    private List<Double> despesasMensais = new ArrayList<>();
-    public void adicionarMembro(Membro membro) {
-        membros.add(membro);
-    }
-    public void adicionarDespesasMensais(double despesas) {
-        despesasMensais.add(despesas);
-    }
-    public double calcularReceitasMensais() {
-        double receitas = 0.0;
-        for (Membro membro : membros) {
-            receitas += membro.getMensalidade();
+
+public class RelatorioEstatisticas {
+    public static class Membro {
+        private String nome;
+        private int frequencia;
+        private double mensalidade;
+
+        public Membro(String nome, int frequencia, double mensalidade) {
+            this.nome = nome;
+            this.frequencia = frequencia;
+            this.mensalidade = mensalidade;
         }
-        return receitas;
-    }
-    public int calcularFrequenciaMensal() {
-        int frequenciaTotal = 0;
-        for (Membro membro : membros) {
-            frequenciaTotal += membro.getFrequencia();
+
+        public String getNome() {
+            return nome;
         }
-        return frequenciaTotal;
+
+        public int getFrequencia() {
+            return frequencia;
+        }
+
+        public double getMensalidade() {
+            return mensalidade;
+        }
     }
-    public double calcularLucroMensal() {
-        double receitas = calcularReceitasMensais();
-        double despesas = despesasMensais.stream().mapToDouble(Double::doubleValue).sum();
-        return receitas - despesas;
+
+    public static class Academia {
+        private List<Membro> membros = new ArrayList<>();
+        private List<Double> despesasMensais = new ArrayList<>();
+
+        public void adicionarMembro(Membro membro) {
+            membros.add(membro);
+        }
+
+        public void adicionarDespesasMensais(double despesas) {
+            despesasMensais.add(despesas);
+        }
+
+        public double calcularReceitasMensais() {
+            double receitas = 0.0;
+            for (Membro membro : membros) {
+                receitas += membro.getMensalidade();
+            }
+            return receitas;
+        }
+
+        public int calcularFrequenciaMensal() {
+            int frequenciaTotal = 0;
+            for (Membro membro : membros) {
+                frequenciaTotal += membro.getFrequencia();
+            }
+            return frequenciaTotal;
+        }
+
+        public double calcularLucroMensal() {
+            double receitas = calcularReceitasMensais();
+            double despesas = despesasMensais.stream().mapToDouble(Double::doubleValue).sum();
+            return receitas - despesas;
+        }
     }
+
     public static void main(String[] args) {
         Academia academia = new Academia();
 
@@ -56,7 +70,7 @@ public class RelatoriosEstatisticas {
         Membro membro2 = new Membro("Larissa", 20, 80.0);
         Membro membro3 = new Membro("Pedro", 22, 80.0);
         Membro membro4 = new Membro("Clara", 20, 80.0);
-        Membro membro5 = new Membro ("Ludmila", 20, 80.0);
+        Membro membro5 = new Membro("Ludmila", 20, 80.0);
 
         academia.adicionarMembro(membro1);
         academia.adicionarMembro(membro2);
