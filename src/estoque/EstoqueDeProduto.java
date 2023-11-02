8fd7073def5225e0816e0527e40dfa5bf98335b8
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-11-02 19:31:48 -0300

    Update EstoqueDeProduto.java
    atualizando, também precisa de uma extensão

--- src/estoque/EstoqueDeProduto.java
+++ src/estoque/EstoqueDeProduto.java
@@ -1,29 +1,32 @@
 package estoque;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class EstoqueDeProduto {
+public class EstoqueDeProdutos {
     private String nome;
     private double preco;
     private int quantidade;
-    public Produto(String nome, double preco, int quantidade) {
+
+    public EstoqueDeProdutos(String nome, double preco, int quantidade) {
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
     }
+
     public String getNome() {
         return nome;
     }
+
     public double getPreco() {
         return preco;
     }
+
     public int getQuantidade() {
         return quantidade;
     }
+
     public void adicionarQuantidade(int quantidade) {
         this.quantidade += quantidade;
     }
+
     public void removerQuantidade(int quantidade) {
         if (this.quantidade >= quantidade) {
             this.quantidade -= quantidade;
@@ -31,33 +34,7 @@ public class EstoqueDeProduto {
             System.out.println("Infelizmente acabou o estoque.");
         }
     }
-}
-public class SistemaAcademia {
-    private Map<String, Produto> estoque = new HashMap<>();
-    public void adicionarProduto(String nome, double preco, int quantidade) {
-        if (estoque.containsKey(nome)) {
-            Produto produtoExistente = estoque.get(nome);
-            produtoExistente.adicionarQuantidade(quantidade);
-        } else {
-            Produto novoProduto = new Produto(nome, preco, quantidade);
-            estoque.put(nome, novoProduto);
-        }
-    }
-    public void venderProduto(String nome, int quantidade) {
-        if (estoque.containsKey(nome)) {
-            Produto produto = estoque.get(nome);
-            produto.removerQuantidade(quantidade);
-            System.out.println("Vendido com sucesso: " + quantidade + " " + nome + "(s) por R$" + (produto.getPreco() * quantidade));
-        } else {
-            System.out.println("Poxa, não encontramos no estoque.");
-        }
-    }
-    public void mostrarEstoque() {
-        System.out.println("Estoque atual:");
-        for (Produto produto : estoque.values()) {
-            System.out.println(produto.getNome() + " - R$" + produto.getPreco() + " - Quantidade: " + produto.getQuantidade());
-        }
-    }
+
     public static void main(String[] args) {
         SistemaAcademia sistema = new SistemaAcademia();
 
