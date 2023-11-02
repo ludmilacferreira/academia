d902891d3900b5b8bd7c8cf3c2ba92538ab2d535
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-11-02 19:25:17 -0300

    Update ControleDePagamento.java
    atualizando, agora vai ser dividida em três classes pra funcionalidade em controler funcionar corretamente

--- src/pagamentos/ControleDePagamento.java
+++ src/pagamentos/ControleDePagamento.java
@@ -1,9 +1,6 @@
 package pagamentos;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ControleDePagamento {
+public class ControleDePagamentos {
     private String nome;
     private double valor;
     private boolean pago;
@@ -35,40 +32,6 @@ public class ControleDePagamento {
     }
 
     public void add(ControleDePagamentos controleDePagamento) {
-    }
-}
-
-class Academia {
-    private List<ControleDePagamentos> controleDePagamentos;
-
-    public Academia() {
-        this.controleDePagamentos = new ArrayList<>();
-    }
-
-    public void adicionarControleDePagamento(ControleDePagamentos controleDePagamentos) {
-        controleDePagamentos.add(controleDePagamentos);
-    }
-
-    public void emitirFatura() {
-        for (ControleDePagamentos controleDePagamento : controleDePagamentos) {
-            if (!controleDePagamento.estaPago()) {
-                System.out.println("Fatura para " + controleDePagamento.getNome() + " no valor de " + controleDePagamento.getValor() + " foi emitida.");
-            }
-        }
-    }
-
-    public void emitirRecibo(ControleDePagamentos controleDePagamento) {
-        if (controleDePagamento.estaPago()) {
-            System.out.println("Recibo para " + controleDePagamento.getNome() + " no valor de " + controleDePagamento.getValor() + " foi emitido.");
-        }
-    }
-
-    public void acompanharPagamentosAtrasados() {
-        for (ControleDePagamentos controleDePagamento : controleDePagamentos) {
-            if (!controleDePagamento.estaPago()) {
-                System.out.println("Pagamento em atraso para " + controleDePagamento.getNome() + " no valor de " + controleDePagamento.getValor());
-            }
-        }
     }
 }
 
