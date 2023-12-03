100763dd95f0e1b99263d3947ea05d342b8b12aa
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-12-03 17:56:44 -0300

    Update ControleDeEstoque.java
    apagando parte duplicada

--- src/estoque/ControleDeEstoque.java
+++ src/estoque/ControleDeEstoque.java
@@ -29,39 +29,4 @@ public class ControleDeEstoque {
         }
     }
 }
-public class ControleEstoque {
-    private Map<String, ControleDeEstoque> estoque;
-
-    public void ControleEstoque() {
-        estoque = new HashMap<>();
-    }
-
-    public void adicionarProduto(ControleDeEstoque produto) {
-        estoque.put(produto.getNome(), produto);
-    }
-
-    public ControleDeEstoque buscarProduto(String nome) {
-        return estoque.get(nome);
-    }
-
-    public void atualizarEstoque(String nome, int quantidade) {
-        ControleDeEstoque produto = buscarProduto(nome);
-        if (produto != null) {
-            produto.adicionarQuantidade(quantidade);
-        } else {
-            System.out.println("Produto não encontrado no estoque.");
-        }
-    }
-
-    public void removerProduto(String nome) {
-        estoque.remove(nome);
-    }
-
-    public void listarProdutosEmEstoque() {
-        System.out.println("Produtos em estoque:");
-        for (ControleDeEstoque produto : estoque.values()) {
-            System.out.println(produto.getNome() + ": " + produto.getQuantidadeEmEstoque() + " unidades");
-        }
-    }
-}
 
