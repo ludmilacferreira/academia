ca368607add3f03e5e4620b8663edd34c0ae2b29
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-12-07 00:05:57 -0300

    Update CadastroDeFuncionarios.java
    tirando o método main de CadastroDeFuncionários

--- src/cadastro/CadastroDeFuncionarios.java
+++ src/cadastro/CadastroDeFuncionarios.java
@@ -41,19 +41,3 @@ public class CadastroDeFuncionarios {
         }
     }
 }
-
-class CadastroFuncionarios {
-    public static void main(String[] args) {
-        CadastroDeFuncionarios cadastro = new CadastroDeFuncionarios();
-
-        CadastroDeFuncionarios.Funcionario funcionario1 = cadastro.new Funcionario("João", "Faxineiro");
-        CadastroDeFuncionarios.Funcionario funcionario2 = cadastro.new Funcionario("Larissa", "Recepcionista");
-        CadastroDeFuncionarios.Funcionario funcionario3 = cadastro.new Funcionario("Pedro", "Instrutor");
-
-        cadastro.adicionarFuncionario(funcionario1);
-        cadastro.adicionarFuncionario(funcionario2);
-        cadastro.adicionarFuncionario(funcionario3);
-
-        cadastro.mostrarFuncionarios();
-    }
-}
