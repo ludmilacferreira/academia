1c749b738c61f0e54a0e7e02826892eb476652e3
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-12-07 00:28:03 -0300

    Update Seguranca.java
    tirando o método main de Seguranca

--- src/segurança/Seguranca.java
+++ src/segurança/Seguranca.java
@@ -1,9 +1,7 @@
 package seguranca;
 
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Seguranca {
     private Map<String, String> users = new HashMap<>();
@@ -16,47 +14,5 @@ public class Seguranca {
     public boolean authenticateUser(String username, String password) {
         String storedPassword = users.get(username);
         return storedPassword != null && storedPassword.equals(password);
-    }
-
-    public static void main(String[] args) {
-        Seguranca securitySystem = new Seguranca();
-        Scanner scanner = new Scanner(System.in);
-
-        Calendar calendar = Calendar.getInstance();
-        int hora = calendar.get(Calendar.HOUR_OF_DAY);
-        String saudacao = "";
-
-        if (hora >= 0 && hora < 12) {
-            saudacao = "Bom dia! ";
-        } else if (hora >= 12 && hora < 18) {
-            saudacao = "Boa tarde! ";
-        } else {
-            saudacao = "Boa noite! ";
-        }
-
-        System.out.println(saudacao + "Vamos treinar?");
-
-        int attempts = 3;
-        while (attempts > 0) {
-            System.out.print("Digite seu nome de usuário: ");
-            String username = scanner.nextLine();
-
-            System.out.print("Digite sua senha: ");
-            String password = scanner.nextLine();
-
-            if (securitySystem.authenticateUser(username, password)) {
-                System.out.println("Autenticação bem-sucedida. Acesso concedido!");
-                break;
-            } else {
-                System.out.println("Autenticação falhou. Tente novamente.");
-                attempts--;
-                System.out.println("Tentativas restantes: " + attempts);
-            }
-        }
-        if (attempts == 0) {
-            System.out.println("Número máximo de tentativas excedido. Conta bloqueada.");
-        }
-
-        scanner.close();
     }
 }
