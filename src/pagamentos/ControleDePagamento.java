e29a249001e42d99fde14b265b9b8a138cc72c8c
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-12-07 00:25:19 -0300

    Update ControleDePagamento.java
    tirando o método main de ControleDePagamento

--- src/pagamentos/ControleDePagamento.java
+++ src/pagamentos/ControleDePagamento.java
@@ -5,14 +5,13 @@ public class ControleDePagamentos {
     private double valor;
     private boolean pago;
 
-    public ControleDePagamentos (String nome, double valor) {
+    public ControleDePagamentos(String nome, double valor) {
         this.nome = nome;
         this.valor = valor;
         this.pago = false;
     }
 
     public ControleDePagamentos() {
-
     }
 
     public String getNome() {
@@ -32,29 +31,5 @@ public class ControleDePagamentos {
     }
 
     public void add(ControleDePagamentos controleDePagamento) {
-    }
-}
-
-class ControlePagamentosAcademia {
-    public static void main(String[] args) {
-        Academia academia = new Academia();
-
-        ControleDePagamentos c1 = new ControleDePagamentos("João", 100.0);
-        ControleDePagamentos c2 = new ControleDePagamentos("Maria", 120.0);
-        ControleDePagamentos c3 = new ControleDePagamentos("Rafaela", 150.0);
-
-        academia.adicionarControleDePagamento(c1);
-        academia.adicionarControleDePagamento(c2);
-        academia.adicionarControleDePagamento(c3);
-
-        c1.pagar();
-        c2.pagar();
-        c3.pagar();
-
-        academia.emitirFatura();
-        academia.emitirRecibo(c1);
-        academia.emitirRecibo(c2);
-        academia.emitirRecibo(c3);
-        academia.acompanharPagamentosAtrasados();
     }
 }
