e6c08c589ddfef3f4b4c371a8e8476f5a42964ac
PedroAndradeXD <147777262+PedroAndradeXD@users.noreply.github.com>
2023-12-08 23:28:33 -0300

    Update CompraDeProdutos.java
    atualizando a classe CompraDeProdutos com uma exception

--- src/pagamentos/CompraDeProdutos.java
+++ src/pagamentos/CompraDeProdutos.java
@@ -1,11 +1,14 @@
 package pagamentos;
 
 import estoque.SistemaAcademia;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class CompraDeProdutos {
 
-    public void realizarCompra() {
+    public void mostrarCompraDeProdutosComPagamento() {
         SistemaAcademia sistema = new SistemaAcademia();
 
         sistema.adicionarProduto("Whey", 99.99, 100);
@@ -14,25 +17,36 @@ public class CompraDeProdutos {
         sistema.adicionarProduto("Suplemento polivitamínico", 80.00, 50);
 
         Scanner scanner = new Scanner(System.in);
-        boolean realizarPagamento = false;
 
         System.out.println("Bem-vindo ao sistema de compras!");
         sistema.mostrarEstoque();
 
+        System.out.print("Digite o nome do produto que deseja comprar (ou 'pagar' para ir ao menu de pagamento): ");
+        String input;
+        boolean realizarPagamento = false;
+
         while (!realizarPagamento) {
-            System.out.print("Digite o nome do produto que deseja comprar (ou 'pagar' para ir ao menu de pagamento): ");
-            String input = scanner.nextLine();
+            input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("pagar")) {
                 realizarPagamento = true;
-                continue;
+                break;
             }
 
-            System.out.print("Digite a quantidade que deseja comprar: ");
-            int quantidade = Integer.parseInt(scanner.nextLine());
+            try {
+                System.out.print("Digite a quantidade que deseja comprar: ");
+                int quantidade = Integer.parseInt(scanner.nextLine());
 
-            sistema.venderProduto(input, quantidade);
-            sistema.mostrarEstoque();
+                sistema.venderProduto(input, quantidade);
+                sistema.mostrarEstoque();
+                System.out.print("Digite o nome do produto que deseja comprar (ou 'pagar' para ir ao menu de pagamento): ");
+            } catch (NumberFormatException e) {
+                System.out.println("Quantidade inválida. Por favor, insira um número inteiro.");
+            } catch (NoSuchElementException e) {
+                System.out.println("Produto não encontrado. Por favor, escolha um produto válido.");
+            } catch (Exception e) {
+                System.out.println("Ocorreu um erro ao processar a compra. Por favor, tente novamente.");
+            }
         }
 
         System.out.println("Chamando o Menu de Pagamento...");
